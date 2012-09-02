b559c1ff95c5a073b2d080ba38371ad75fc95a8e
Chuck Ocheret <ocheret@panix.com>
2012-09-02 01:41:13 -0400

    Cleaned up to fit in 80 column margin.  Added Clear, AddScaledVectorTo, CrossProduct, CrossProductTo, and TrimTo

--- RPPhysicsOC/RPVector3.h
+++ RPPhysicsOC/RPVector3.h
@@ -24,7 +24,8 @@ static __inline__ RPVector3 RPVector3Make(RPReal x, RPReal y, RPReal z)
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3Set(RPVector3 *me, RPReal x, RPReal y, RPReal z)
+static __inline__ RPVector3 *RPVector3Set(RPVector3 *me,
+                                          RPReal x, RPReal y, RPReal z)
 {
     me->x = x; me->y = y; me->z = z;
     return me;
@@ -36,9 +37,15 @@ static __inline__ RPVector3 RPVector3MakeWithArray(RPReal values[3])
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3SetWithArray(RPVector3 *me, RPReal values[3])
+static __inline__ RPVector3 *RPVector3SetWithArray(RPVector3 *me,
+                                                   RPReal values[3])
 {
     me->x = values[0]; me->y = values[1]; me->z = values[2];
+    return me;
+}
+
+static __inline__ RPVector3 *RPVector3Clear(RPVector3 *me) {
+    me->x = me->y = me->z = 0;
     return me;
 }
 
@@ -56,7 +63,8 @@ static __inline__ RPVector3 *RPVector3NegateOf(RPVector3 *me)
 
 static __inline__ RPVector3 RPVector3Add(RPVector3 *left, RPVector3 *right)
 {
-    RPVector3 a = { left->x + right->x, left->y + right->y, left->z + right->z };
+    RPVector3 a = {
+        left->x + right->x, left->y + right->y, left->z + right->z };
     return a;
 }
 
@@ -66,37 +74,55 @@ static __inline__ RPVector3 *RPVector3AddTo(RPVector3 *me, RPVector3 *right)
     return me;
 }
 
+static __inline__ RPVector3 *RPVector3AddScaledVectorTo(RPVector3 *me,
+                                                        RPVector3 *vector,
+                                                        RPReal scalar)
+{
+    me->x += vector->x * scalar;
+    me->y += vector->x * scalar;
+    me->z += vector->z * scalar;
+    return me;
+}
+
 static __inline__ RPVector3 RPVector3Subtract(RPVector3 *left, RPVector3 *right)
 {
-    RPVector3 a = { left->x - right->x, left->y - right->y, left->z - right->z };
+    RPVector3 a = {
+        left->x - right->x, left->y - right->y, left->z - right->z };
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3SubtractFrom(RPVector3 *me, RPVector3 *right)
+static __inline__ RPVector3 *RPVector3SubtractFrom(RPVector3 *me,
+                                                   RPVector3 *right)
 {
     me->x -= right->x; me->y -= right->y; me->z -= right->z;
     return me;
 }
 
-static __inline__ RPVector3 RPVector3Multiply(RPVector3 *left, RPVector3 *right)
+static __inline__ RPVector3 RPVector3Multiply(RPVector3 *left,
+                                              RPVector3 *right)
 {
-    RPVector3 a = { left->x * right->x, left->y * right->y, left->z * right->z };
+    RPVector3 a = {
+        left->x * right->x, left->y * right->y, left->z * right->z };
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3MultiplyBy(RPVector3 *me, RPVector3 *right)
+static __inline__ RPVector3 *RPVector3MultiplyBy(RPVector3 *me,
+                                                 RPVector3 *right)
 {
     me->x *= right->x; me->y *= right->y; me->z *= right->z;
     return me;
 }
 
-static __inline__ RPVector3 RPVector3Divide(RPVector3 *left, RPVector3 *right)
+static __inline__ RPVector3 RPVector3Divide(RPVector3 *left,
+                                            RPVector3 *right)
 {
-    RPVector3 a = { left->x / right->x, left->y / right->y, left->z / right->z };
+    RPVector3 a = {
+        left->x / right->x, left->y / right->y, left->z / right->z };
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3DivideBy(RPVector3 *me, RPVector3 *right)
+static __inline__ RPVector3 *RPVector3DivideBy(RPVector3 *me,
+                                               RPVector3 *right)
 {
     me->x /= right->x; me->y /= right->y; me->z /= right->z;
     return me;
@@ -120,7 +146,8 @@ static __inline__ RPVector3 RPVector3SubtractScalar(RPVector3 *w, RPReal value)
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3SubtractScalarFrom(RPVector3 *me, RPReal value)
+static __inline__ RPVector3 *RPVector3SubtractScalarFrom(RPVector3 *me,
+                                                         RPReal value)
 {
     me->x -= value; me->y -= value; me->z -= value;
     return me;
@@ -132,7 +159,8 @@ static __inline__ RPVector3 RPVector3MultiplyScalar(RPVector3 *w, RPReal value)
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3MultiplyScalarBy(RPVector3 *me, RPReal value)
+static __inline__ RPVector3 *RPVector3MultiplyScalarBy(RPVector3 *me,
+                                                       RPReal value)
 {
     me->x *= value; me->y *= value; me->z *= value;
     return me;
@@ -144,7 +172,8 @@ static __inline__ RPVector3 RPVector3DivideScalar(RPVector3 *w, RPReal value)
     return a;
 }
 
-static __inline__ RPVector3 *RPVector3DivideScalarBy(RPVector3 *me, RPReal value)
+static __inline__ RPVector3 *RPVector3DivideScalarBy(RPVector3 *me,
+                                                     RPReal value)
 {
     me->x /= value; me->y /= value; me->z /= value;
     return me;
@@ -159,7 +188,9 @@ static __inline__ RPVector3 RPVector3Maximum(RPVector3 *left, RPVector3 *right)
     return max;
 }
 
-static __inline__ RPVector3 *RPVector3MaximumTo(RPVector3 *me, RPVector3 *left, RPVector3 *right)
+static __inline__ RPVector3 *RPVector3MaximumTo(RPVector3 *me,
+                                                RPVector3 *left,
+                                                RPVector3 *right)
 {
     me->x = (left->x > right->x) ? left->x : right->x;
     me->y = (left->y > right->y) ? left->y : right->y;
@@ -167,7 +198,8 @@ static __inline__ RPVector3 *RPVector3MaximumTo(RPVector3 *me, RPVector3 *left, 
     return me;
 }
 
-static __inline__ RPVector3 RPVector3Minimum(RPVector3 *left, RPVector3 *right)
+static __inline__ RPVector3 RPVector3Minimum(RPVector3 *left,
+                                             RPVector3 *right)
 {
     RPVector3 min;
     min.x = (left->x < right->x) ? left->x : right->x;
@@ -176,7 +208,9 @@ static __inline__ RPVector3 RPVector3Minimum(RPVector3 *left, RPVector3 *right)
     return min;
 }
 
-static __inline__ RPVector3 *RPVector3MinimumTo(RPVector3 *me, RPVector3 *left, RPVector3 *right)
+static __inline__ RPVector3 *RPVector3MinimumTo(RPVector3 *me,
+                                                RPVector3 *left,
+                                                RPVector3 *right)
 {
     me->x = (left->x < right->x) ? left->x : right->x;
     me->y = (left->y < right->y) ? left->y : right->y;
@@ -185,7 +219,8 @@ static __inline__ RPVector3 *RPVector3MinimumTo(RPVector3 *me, RPVector3 *left, 
 }
 
 
-static __inline__ BOOL RPVector3AllEqualToVector3(RPVector3 *left, RPVector3 *right)
+static __inline__ BOOL RPVector3AllEqualToVector3(RPVector3 *left,
+                                                  RPVector3 *right)
 {
     return (left->x == right->x && left->y == right->y && left->z == right->z);
 }
@@ -195,7 +230,8 @@ static __inline__ BOOL RPVector3AllEqualToScalar(RPVector3 *a, RPReal value)
     return (a->x == value && a->y == value && a->z == value);
 }
 
-static __inline__ BOOL RPVector3AllGreaterThanVector3(RPVector3 *left, RPVector3 *right)
+static __inline__ BOOL RPVector3AllGreaterThanVector3(RPVector3 *left,
+                                                      RPVector3 *right)
 {
     return (left->x > right->x && left->y > right->y && left->z > right->z);
 }
@@ -205,12 +241,14 @@ static __inline__ BOOL RPVector3AllGreaterThanScalar(RPVector3 *a, RPReal value)
     return (a->x > value && a->y > value && a->z > value);
 }
 
-static __inline__ BOOL RPVector3AllGreaterThanOrEqualToVector3(RPVector3 *left, RPVector3 *right)
+static __inline__ BOOL RPVector3AllGreaterThanOrEqualToVector3(RPVector3 *left,
+                                                               RPVector3 *right)
 {
     return (left->x >= right->x && left->y >= right->y && left->z >= right->z);
 }
 
-static __inline__ BOOL RPVector3AllGreaterThanOrEqualToScalar(RPVector3 *a, RPReal value)
+static __inline__ BOOL RPVector3AllGreaterThanOrEqualToScalar(RPVector3 *a,
+                                                              RPReal value)
 {
     return (a->x >= value && a->y >= value && a->z >= value);
 }
@@ -218,6 +256,27 @@ static __inline__ BOOL RPVector3AllGreaterThanOrEqualToScalar(RPVector3 *a, RPRe
 static __inline__ RPReal RPVector3DotProduct(RPVector3 *left, RPVector3 *right)
 {
     return left->x * right->x + left->y * right->y + left->z * right->z;
+}
+
+static __inline__ RPVector3 RPVector3CrossProduct(RPVector3 *left,
+                                                  RPVector3 *right)
+{
+    RPVector3 a = {
+        left->y * right->z - left->z * right->y,
+        left->z * right->x - left->x * right->z,
+        left->x * right->y - left->y * right->x
+    };
+    return a;
+}
+
+static __inline__ RPVector3 *RPVector3CrossProductTo(RPVector3 *me,
+                                                     RPVector3 *left,
+                                                     RPVector3 *right)
+{
+    me->x = left->y * right->z - left->z * right->y;
+    me->y = left->z * right->x - left->x * right->z;
+    me->z = left->x * right->y - left->y * right->x;
+    return me;
 }
 
 static __inline__ RPReal RPVector3LengthSquared(RPVector3 *a)
@@ -240,8 +299,14 @@ static __inline__ RPVector3 RPVector3Normalize(RPVector3 *w)
 static __inline__ RPVector3 *RPVector3NormalizeTo(RPVector3 *me, RPVector3 *w)
 {
     RPReal scale = RPRealValue(1.0) / RPVector3Length(w);
-    me->x = w->x * scale;
-    me->y = w->y * scale;
-    me->z = w->z * scale;
+    me->x = w->x * scale; me->y = w->y * scale; me->z = w->z * scale;
+    return me;
+}
+
+static __inline__ RPVector3 *RPVector3TrimTo(RPVector3 *me, RPReal size)
+{
+    if (RPVector3LengthSquared(me) > size * size) {
+        RPVector3MultiplyScalarBy(RPVector3NormalizeTo(me, me), size);
+    }
     return me;
 }
